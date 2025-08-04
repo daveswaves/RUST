eb0074b2d58f0d3166e498654fedc7c23bc70964
David <daveswaves.2008@gmail.com>
2025-08-04 16:02:34 +0100

    extract_select_skus_to_tsv project

---
+++ rust_projects/extract_select_skus_to_tsv/src/main.rs
@@ -0,0 +1,99 @@
+/* use std::fs::read_to_string;
+
+fn main() {
+    // println!("Hello, world!");
+} */
+
+
+use std::fs::read_to_string;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufWriter, Write, BufRead, BufReader, Result};
+
+/* use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    // File hosts.txt must exist in the current path
+    if let Ok(lines) = read_lines("./ignored_skus.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.map_while(Result::ok) {
+            println!("{}", line);
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+} */
+
+
+fn main() {
+    // let map = file_to_hashmap("./ignored_skus.txt").unwrap();
+    let map = match file_to_hashmap("./files/ignored_skus.txt") {
+        Ok(m) => m,
+        Err(_e) => {
+            println!("Required file does not exist. Extract 'files.7z' to create required files.");
+            return
+        }
+    };
+    
+    // if map.get("'13701A'").is_some() {
+        //     println!("Remove: '13701A'"); // {:#?}
+    // }
+    
+    let tsv = read_lines("./files/new_skus_2025.08.04.tsv");
+
+    let new_list = tsv.iter()
+        // .filter(|s|  map.get(*s).is_some())
+        .filter(|line| map.keys().any(|key| line.contains(key)))
+        .cloned()
+        .collect::<Vec<String>>();
+
+    write_lines_to_file(&new_list, "./files/ignored_matches.tsv").unwrap();
+    // println!("{:#?}", new_list);
+}
+
+fn write_lines_to_file(lines: &[String], filename: &str) -> std::io::Result<()> {
+    let file = File::create(filename)?;
+    let mut writer = BufWriter::new(file);
+
+
+    writeln!(writer, "{}", "keyphrase\tsku\ttitle".to_string())?;
+    for line in lines {
+        writeln!(writer, "{}", line)?;
+    }
+
+    Ok(())
+}
+
+fn file_to_hashmap(filename: &str) -> Result<HashMap<String, i32>> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut map = HashMap::new();
+
+    for line_result in reader.lines() {
+        let line = line_result?;
+        let key = line.trim().to_string();
+        if !key.is_empty() {
+            map.insert(key, 1);
+        }
+    }
+
+    Ok(map)
+}
+
+#[allow(dead_code)]
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename) 
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
--- rust_projects/sandbox/src/main.rs
+++ rust_projects/sandbox/src/main.rs
@@ -1,32 +1,5 @@
-use std::io::{self, Write};
-
 fn main() {
-    input_with_prompt();
-}
-
-fn input_with_prompt() {
-    let mut input1 = String::new();
-    let mut input2 = String::new();
-    
-    print!("Enter 1st number: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    
-    io::stdin()
-        .read_line(&mut input1)
-        .expect("Failed to read line!");
-    
-    let num1: i64 = input1.trim().parse().unwrap();
-    
-    print!("Enter 2nd number: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    
-    io::stdin()
-        .read_line(&mut input2)
-        .expect("Failed to read line!");
-    
-    let num2: i64 = input2.trim().parse().unwrap();
-    
-    let ans = num1 + num2;
-    
-    println!("Sum of 2 numbers ({} + {}): {}", num1, num2, ans);
+    println!("Okay");
+    println!("{}", "Okay".to_string());
+    println!("{}", String::from("Okay"));
 }
